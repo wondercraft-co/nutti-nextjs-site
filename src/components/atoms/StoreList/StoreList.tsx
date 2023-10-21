76109aaaab57c1533def82dba0a9de1559552970
Juan Pablo Solano <hello@wondercraft.co>
2023-10-21 14:03:40 -0500

    accordion

--- src/components/atoms/StoreList/StoreList.tsx
+++ src/components/atoms/StoreList/StoreList.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { Store } from "@/components/molecules/StoreLocatorContainer/StoreLocatorContainer";
+import * as Accordion from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
-import { Fragment, useCallback } from "react";
+import { useCallback } from "react";
 
 interface IStoreListProps {
   stores: Store[];
@@ -25,42 +27,57 @@ const StoreList = ({ stores }: IStoreListProps) => {
       return store.city;
     });
     return (
-      <figure>
+      <Accordion.Root
+        className="bg-mauve6 rounded-md shadow-black/5"
+        type="single"
+        // defaultValue=}
+        collapsible
+      >
         {Object.entries(groupedByCity).map(([city, stores], index) => {
           return (
-            <div key={city} className="px-5">
-              <figcaption className="text-2xl font-bold mt-10 mb-4 capitalize">
+            <Accordion.AccordionItem
+              className="border-b border-b-yellow/50"
+              value={city}
+              key={city}
+            >
+              <Accordion.AccordionTrigger className="text-2xl font-bold p-3 capitalize flex items-center justify-between w-full">
                 {city}
-              </figcaption>
-              <ul className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-5">
-                {stores.map((store) => (
-                  <li key={store._id}>
-                    <div className="bg-white shadow-md rounded-lg overflow-hidden border border-gray-200 p-4">
-                      <h3 className="font-semibold text-lg">{store.title}</h3>
-                      <p>{store.address}</p>
-                      <p>
-                        <Link
-                          className="text-b-green"
-                          href={`tel:${store.phone}`}
-                        >
-                          {store.phone}
-                        </Link>
-                      </p>
-                      {store.url ? (
+                <ChevronDownIcon
+                  className="text-violet10 ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180"
+                  aria-hidden
+                />
+              </Accordion.AccordionTrigger>
+              <Accordion.AccordionContent className=" data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp overflow-hidden">
+                <ul className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-5 pb-5 px-3">
+                  {stores.map((store) => (
+                    <li key={store._id}>
+                      <div className="bg-white shadow-md rounded-lg overflow-hidden border border-gray-200 p-4">
+                        <h3 className="font-semibold text-lg">{store.title}</h3>
+                        <p>{store.address}</p>
                         <p>
-                          <Link className="text-b-green" href={store.url}>
-                            Sitio web
+                          <Link
+                            className="text-b-green"
+                            href={`tel:${store.phone}`}
+                          >
+                            {store.phone}
                           </Link>
                         </p>
-                      ) : null}
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+                        {store.url ? (
+                          <p>
+                            <Link className="text-b-green" href={store.url}>
+                              Sitio web
+                            </Link>
+                          </p>
+                        ) : null}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </Accordion.AccordionContent>
+            </Accordion.AccordionItem>
           );
         })}
-      </figure>
+      </Accordion.Root>
     );
   }, [stores]);
   return <div>{storeListRender()}</div>;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,14 @@ const config: Config = {
           from: { opacity: "0", transform: "translateX(-2px)" },
           to: { opacity: "1", transform: "translateX(0)" },
         },
+        slideDown: {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        slideUp: {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
         overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
@@ -90,6 +98,8 @@ const config: Config = {
         slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
         slideRightAndFade:
           "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+        slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
       },
     },
   },
