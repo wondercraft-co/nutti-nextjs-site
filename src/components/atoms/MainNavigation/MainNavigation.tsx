15eadfdf4b578cc3bfb24c2ece96dfa518dabf8b
Juan Pablo Solano <hello@wondercraft.co>
2023-10-21 16:39:49 -0500

    nav scroll

--- src/components/atoms/MainNavigation/MainNavigation.tsx
+++ src/components/atoms/MainNavigation/MainNavigation.tsx
@@ -1,12 +1,28 @@
+"use client";
 import { navigationItems } from "@/utils/constants";
 import Image from "next/image";
 import Link from "next/link";
 import MainNavigationMobileMenu from "../MainNavigationMobileMenu";
+import useScrollDirection from "@/utils/useScrollDirection";
+import { useWindowScroll } from "@uidotdev/usehooks";
+import { cn } from "@/utils";
 interface IMainNavigationProps {}
 
 const MainNavigation = ({}: IMainNavigationProps) => {
+  const scrollDirection = useScrollDirection();
+  const [{ y }] = useWindowScroll();
   return (
-    <header className="w-full absolute top-0">
+    <header
+      className={cn(
+        "w-full fixed top-0 transition-all duration-300 z-20 ",
+        {
+          "bg-b-back": y && y > 100,
+        }
+      )}
+      style={{
+        transform: `translateY(${scrollDirection === "up" ? 0 : "-100%"})`,
+      }}
+    >
       <div className="container py-5 px-5">
         <div className="flex justify-between">
           <Link href="/">
---
+++ src/utils/useScrollDirection.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+export const SCROLL_UP = "up";
+export const SCROLL_DOWN = "down";
+
+type ScrollDirection = typeof SCROLL_UP | typeof SCROLL_DOWN;
+
+const useScrollDirection = ({
+  initialDirection = SCROLL_UP,
+  thresholdPixels = 10,
+}: {
+  initialDirection?: ScrollDirection;
+  thresholdPixels?: number;
+} = {}) => {
+  const [scrollDir, setScrollDir] = useState<ScrollDirection>(initialDirection);
+
+  useEffect(() => {
+    const threshold = thresholdPixels || 0;
+    let lastScrollY = window.scrollY;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const scrollY = window.scrollY;
+
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        // We haven't exceeded the threshold
+        ticking = false;
+        return;
+      }
+
+      setScrollDir(scrollY > lastScrollY ? SCROLL_DOWN : SCROLL_UP);
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [initialDirection, thresholdPixels]);
+
+  return scrollDir;
+};
+
+export default useScrollDirection;
