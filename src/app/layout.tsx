1ed682710f07d53cbcf952225811cda602720901
Juan Pablo Solano <hello@wondercraft.co>
2023-10-21 12:05:23 -0500

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import localFont from "next/font/local";
 
-// Font files can be colocated inside of `pages`
-const hkGrotesk = localFont({
-  src: [
-    {
-      path: "../fonts/HKGrotesk-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../fonts/HKGrotesk-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../fonts/HKGrotesk-Semibold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../fonts/HKGrotesk-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  variable: "--font-hk-grotesk",
-});
 
 export const metadata: Metadata = {
   title: "Nutti - Cremas de nueces",
@@ -42,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${hkGrotesk.variable} font-sans antialiased bg-b-back`}>{children}</body>
+      <body className={`font-sans antialiased bg-b-back`}>{children}</body>
     </html>
   );
 }
