bce00bf9fe40bf13ae78e6fdbadc210102978838
Juan Pablo Solano <hello@wondercraft.co>
2023-10-27 18:53:16 -0500

    og images

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -19,11 +19,51 @@ export async function GET(request: Request) {
           height: "100%",
           padding: "50px 200px",
           textAlign: "center",
+          display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        Recetas Saludables
+        <img
+          width="871"
+          height="630"
+          src={`https://www.nutti.co/ogbg.png`}
+          alt=""
+          style={{
+            position: "absolute",
+            zIndex: 0,
+            width: "871px",
+            top: 0,
+            right: 0,
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            zIndex: 10,
+            width: "450px",
+            top: 0,
+            left: 0,
+            height: "630px",
+            background: "white",
+          }}
+        >
+          <img
+            //100 x36
+            width="150"
+            height="54"
+            src={`https://www.nutti.co/nutti-logo.svg`}
+            alt=""
+            style={{
+              marginBottom: 40,
+            }}
+          />
+          {title ? title : "Cremas de nueces 100% naturales"}
+        </div>
       </div>
     ),
     {
